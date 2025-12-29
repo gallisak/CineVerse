6de3922c598ff993ffe4abe5a9f08ad60d1e9b32
Andrii Halchyshak <agalchisak@gmail.com>
2025-12-29 03:12:03 +0200

    style: remove extra emoji

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -34,9 +34,7 @@ export function ProfilePage() {
       <div className="container mx-auto px-6 py-24">
         <div className="flex flex-col md:flex-row items-start md:items-center justify-between mb-12 gap-4">
           <div>
-            <h1 className="text-3xl md:text-4xl font-bold mb-2">
-              My Tickets 🎟️
-            </h1>
+            <h1 className="text-3xl md:text-4xl font-bold mb-2">My Tickets</h1>
             <p className="text-gray-400">
               Account: <span className="text-cyan-400">{email}</span>
             </p>
